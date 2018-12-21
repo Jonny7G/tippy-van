92777030babc8de7921985d4bc46a948fb0aa99b
Jonny7G <45757402+Jonny7G@users.noreply.github.com>
2018-12-21 04:36:32 -0600

    changed background scrolling to be done through texture offset

---
+++ Assets/ScrollTexture.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollTexture : MonoBehaviour
+{
+    [SerializeField]private Renderer myRend;
+    [SerializeField] private Vector3Reference worldDirection;
+    [SerializeField] private FloatReference worldSpeed;
+    [Range(0,1)]
+    [SerializeField] private float moveFactor;
+    private string textName;
+    private Vector2 uvOffset = Vector2.zero;
+    float x = 0;
+    private void Start()
+    {
+        textName = myRend.sharedMaterial.mainTexture.name;
+    }
+
+    void Update()
+    {
+        uvOffset -= (Vector2)worldDirection.value.normalized * Time.deltaTime*worldSpeed.value* moveFactor;
+        myRend.material.mainTextureOffset = (uvOffset);
+    }
+}
